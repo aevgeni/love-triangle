cea0f82d675896a5efd23cb8ae5364b5986b8554
AEvgeni <eslilolko@mail.ru>
2019-03-19 22:31:22 +0300

    feat: task complet

--- src/index.js
+++ src/index.js
@@ -4,4 +4,19 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
+  let first;
+  let second;
+  let third;
+  let count = 0;
+
+  for (let i = 0; i < preferences.length; i++) {
+    first = preferences[i];
+    second = preferences[first-1];
+    third = preferences[second-1];
+    if (first !== second && second !== third && (third - 1) === i) {
+      count++;
+    }
+  }
+
+  return (count/3);
 };
